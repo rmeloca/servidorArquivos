6d9a21e2f856f3c78ab8f670b20a35aba29638f9
romulo <romulo@meloca>
2015-11-11 21:36:37 -0200

    atualizacao do pofesso

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC=gcc
-LDFLAGS=
+LDFLAGS=-lpthread
 CFLAGS=-O0 -ggdb
 
 all: 
--- server.c
+++ server.c
@@ -4,8 +4,48 @@
 #include <unistd.h>
 #include <stdint.h>
 #include "connection.h"
+#include <pthread.h>
 
 #define BUFFER_SIZE 1024
+
+void* echoThread(void* args){
+    
+    connection_t* connection;
+    
+    connection = (connection_t*)args;
+    
+    //Buffer usado para receber e enviar echo
+    uint8_t buffer[BUFFER_SIZE];
+    
+    //Enviar uma mensagem de boas-vindas ao cliente
+    const char* hello = "Servidor Echo v0.01 -- Bem Vindo(a)!";
+    CONN_send(connection, (void*)hello, strlen(hello), 0);
+    
+    //A idéia de um servidor echo é simplesmente mandar de volta a mensagem recebida
+    while(1){
+        //aguardar a mensagem
+        CONN_receive(connection, buffer, BUFFER_SIZE, 0);
+
+        //imprimir a mensagem recebida e o tamanho da mensagem
+        printf("recebi '%s' do cliente (%s:%s)... (len = %d)\n", 
+                buffer,
+                CONN_getPeerName(connection),
+                CONN_getPeerPort(connection),
+                strlen(buffer));
+        
+        //enviar de volta a mensagem recebida.
+        CONN_send(connection, buffer, strlen(buffer)+1, 0);
+        
+        //detectar se a mensagem "sair" foi enviada.. se sim, fechar o servidor
+        if(!strcmp(buffer, "sair")){
+            break;
+        }
+    }    
+    
+    //fechar a conexão
+    CONN_close(connection);
+    
+}
 
 int main(int argc, char** argv){
 
@@ -14,9 +54,6 @@ int main(int argc, char** argv){
     
     //Conexão ao cliente
     connection_t* connection;
-    
-    //Buffer usado para receber e enviar echo
-    uint8_t buffer[BUFFER_SIZE];
     
     //Porta a ser usada pelo servidor... (use portas altas (10000+) para evitar conflitos com serviços já sendo executados)
     char* port;
@@ -33,44 +70,27 @@ int main(int argc, char** argv){
     //Abrir socket para arguardar conexões
     listenSock = CONN_listenTo((char*)port);
     
-    //Aceitar conexões. Esta função retorna uma conexão (usando um novo socket) ou NULL.
-    //listenSocket ainda pode ser chamado para aceitar outras conexões!
-    connection = CONN_accept(listenSock);
-  
-    //Verificar se houve sucesso ao conectar
-    if(!connection){
-        fprintf(stderr, "Não foi possível conectar ao cliente remoto na porta %s!\n", port);
-        return(EXIT_FAILURE);
-    }
-    
-    //Enviar uma mensagem de boas-vindas ao cliente
-    const char* hello = "Servidor Echo v0.01 -- Bem Vindo(a)!";
-    CONN_send(connection, (void*)hello, strlen(hello), 0);
-    
-    //A ideia de um servidor echo é simplesmente mandar de volta a mensagem recebida
     while(1){
-        //aguardar a mensagem
-        CONN_receive(connection, buffer, BUFFER_SIZE, 0);
+        //Aceitar conexões. Esta função retorna uma conexão (usando um novo socket) ou NULL.
+        //listenSocket ainda pode ser chamado para aceitar outras conexões!
+        connection = CONN_accept(listenSock);
 
-        //imprimir a mensagem recebida e o tamanho da mensagem
-        printf("recebi '%s' do cliente... (len = %d)\n", buffer, strlen(buffer));
-        
-        //enviar de volta a mensagem recebida.
-        CONN_send(connection, buffer, strlen(buffer)+1, 0);
-        
-        //detectar se a mensagem "sair" foi enviada.. se sim, fechar o servidor
-        if(!strcmp(buffer, "sair")){
-            break;
+        //Verificar se houve sucesso ao conectar
+        if(!connection){
+            fprintf(stderr, "Não foi possível conectar ao cliente remoto na porta %s!\n", port);
+            return(EXIT_FAILURE);
         }
+
+        pthread_t* t;
+        t = malloc(sizeof(pthread_t));
+        
+        pthread_create(t, NULL, echoThread, (void*) connection);
     }
-    
-    //fechar a conexão
-    CONN_close(connection);
     
     //fechar o socket
     close(listenSock);
     
-    return EXIT_FAILURE;
+    return EXIT_SUCCESS;
     
 }
 
